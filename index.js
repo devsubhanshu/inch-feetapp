1045fd55d3d32635c06c342460ccec25fb8b79c1
devsubhanshu <subhanshukumargupta41@gmail.com>
2024-10-08 22:37:07 +0530

    gggggg

---
+++ index.js
@@ -0,0 +1,30 @@
+const inch = document.getElementById("inch")
+const feet = document.getElementById("feet")
+
+
+
+feet.addEventListener("input" , function(){
+let f =this.value;
+let i=f*12;
+inch.value=i;
+
+i
+
+})
+
+inch.addEventListener("input" , function(){
+let i=this.value;
+let f=i/12;
+if(!Number.isInteger(f)){
+    f=f.toFixed(2);
+}
+feet.value=f;
+
+
+});
+
+
+
+
+
+
